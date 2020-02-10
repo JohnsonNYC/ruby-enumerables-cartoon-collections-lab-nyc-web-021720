16a70879cbee0aa6f9fb4b0f6a92019e2d09f95b
Johnson Kow <jkow95@gmail.com>
2020-02-10 19:48:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(array)# code an argument here
   #  i += 1 
   #end 
   
-  array.each_with_index {|item,index| puts "#{index + 1}. #{item}"
+  array.each_with_index {|item,index| puts "#{index + 1}. #{item}"}
 end
 
 def summon_captain_planet(array)# code an argument here
